9ef896a7e299ad733d75917d3c0061c2a0a56acb
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-08-20 17:21:36 -0700

    stubs created and connected, views created and rendered

--- controllers/articles.js
+++ controllers/articles.js
@@ -4,7 +4,19 @@ const router = express.Router();
 
 // Route
 router.get('/', (req, res) => {
-  res.send('articles home');
+  res.render('articles/index');
+});
+
+router.get('/new', (req, res) => {
+  res.send('new article');
+});
+
+router.get('/:id', (req, res) => {
+  res.send('article id');
+});
+
+router.post('/', (req, res) => {
+  res.send('posted article');
 });
 
 module.exports = router;
--- controllers/authors.js
+++ controllers/authors.js
@@ -4,7 +4,19 @@ const router = express.Router();
 
 // Route
 router.get('/', (req, res) => {
-  res.send('authors home');
+  res.render('authors/index');
+});
+
+router.get('/new', (req, res) => {
+  res.render('authors/new');
+});
+
+router.get('/:id', (req, res) => {
+  res.send('author id');
+});
+
+router.post('/', (req, res) => {
+  res.send('posted author');
 });
 
 module.exports = router;
