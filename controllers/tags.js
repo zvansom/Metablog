cac6bd210c442bdedf97d959774b6f76c178779e
Zach VanSomeren <zvansomeren@gmail.com>
2018-08-22 11:26:19 -0700

    Added put and delete routes

--- controllers/tags.js
+++ controllers/tags.js
@@ -1,4 +1,5 @@
 // Requires
+const async = require('async');
 const db = require('../models');
 const express = require('express');
 const router = express.Router();
@@ -8,6 +9,16 @@ router.get('/', (req, res) => {
   .then( tags => {
     res.render('tags/index', { tags });
   }).catch( err => { res.render('error'); });
+});
+
+router.get('/edit/:id', (req, res) => {
+  db.tag.findById(req.params.id)
+  .then( foundTag => { res.render('tags/edit', { tag: foundTag }); })
+  .catch( err => { res.render('error') });
+});
+
+router.put('/:id', (req, res) => {
+  res.send(req.body);
 });
 
 router.get('/:id', (req, res) => {
@@ -20,7 +31,22 @@ router.get('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  res.send('delete');
-})
+  db.tag.findOne({
+    where: { id: req.params.id },
+    include: [db.article]
+  }).then( foundTag => {
+    async.forEach(foundTag.articles, function(a, done){
+      // Runs for each article
+      foundTag.removeArticle(a)
+      .then( () => { done() });
+    }, function(){
+      // Runs when everything is done.
+      // References in the join table are deleted. Can proceed with deleting the tag itself.
+      db.tag.destroy({ where: { id: req.params.id } })
+      .then( () => { res.send('Deleted') })
+      .catch( err => { res.status(500).send('Oh noooooooo!'); })
+    });
+  }).catch( err => { res.status(500).send('Oh nooooooooo!'); });
+});
 
 module.exports = router;
--- public/js/ajax-calls.js
+++ public/js/ajax-calls.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-  console.log('jquery!');
+  $('.delete-btn').click(function(e) {
+    e.preventDefault();
+    const url = $(this).attr('href');
+
+    $.ajax({
+      url,
+      method: 'DELETE'
+    }).done(function(res) {
+      // console.log('success:', res);
+      window.location = '/' + url.split('/')[1];
+    }).fail(function(err) {
+      console.log('error', err);
+    });
+  });
+
+  $('#edit-tag').submit(function(e) {
+    e.preventDefault();
+    console.log('about to submit PUT req');
+    $.ajax({
+      url: $(this).attr('action'),
+      method: 'PUT',
+      data: $(this).serialize()
+    }).done(function(res) {
+      console.log('success', res);
+    }).fail(function(err) {
+      console.log('error', err);
+    });
+  });
+
 });
